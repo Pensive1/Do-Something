b5a1303784d1ee532457515403cd5d8b1e4200c2
Richard Acquaye <racquaye89@gmail.com>
2023-02-23 13:41:31 +0000

    Core functionality

---
+++ scripts/script.js
@@ -0,0 +1,115 @@
+const refreshBtn = document.querySelector(".footer__form");
+const randomiseBtn = document.querySelector(".footer__randomise");
+
+const people = document.querySelector(".footer__people");
+const category = document.querySelector(".footer__cat");
+const suggestion = document.querySelector(".suggestion__txt");
+
+const quoteTxt = document.querySelector(".motivation__phrase");
+const quoteAuthor = document.querySelector(".motivation__author");
+
+// Number Filter
+people.addEventListener("keyup", (event) => {
+  const participants = event.target.value;
+
+  if (participants !== "" && category.value === "") {
+    getSuggestion(
+      `https://www.boredapi.com/api/activity?participants=${participants}`
+    );
+  } else if (participants !== "" && people.value !== "") {
+    getSuggestion(
+      `https://www.boredapi.com/api/activity?participants=${participants}&type=${category.value}`
+    );
+  }
+
+  //   checkFilters();
+});
+
+// Category Filter
+category.addEventListener("change", (event) => {
+  const type = event.target.value;
+
+  if (type !== "" && people.value == "") {
+    getSuggestion(`https://www.boredapi.com/api/activity?type=${type}`);
+    // console.log(people.value);
+  } else if (type !== "" && people.value !== "") {
+    getSuggestion(
+      `https://www.boredapi.com/api/activity?participants=${people.value}&type=${type}`
+    );
+  }
+});
+
+refreshBtn.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (people.value !== "" && category !== "") {
+    getSuggestion(
+      `https://www.boredapi.com/api/activity?participants=${people.value}&type=${category.value}`
+    );
+  }
+
+  //   checkFilters();
+});
+
+//Randomise Button
+randomiseBtn.addEventListener("click", () => {
+  people.value = "";
+  category.value = "";
+  getSuggestion("https://www.boredapi.com/api/activity");
+});
+
+//ACTIVITY API CALL
+const getSuggestion = (url) => {
+  axios
+    .get(url)
+    .then((result) => {
+      if (result.data.activity === undefined) {
+        return (suggestion.innerText = "I have no idea 😔");
+      }
+
+      suggestion.innerText = result.data.activity;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+getSuggestion("https://www.boredapi.com/api/activity");
+
+const checkFilters = () => {
+  const pplAmount = people.value;
+  const catSelection = category.value;
+
+  //if both aren't empty
+  if (pplAmount !== "" && catSelection !== "") {
+    return getSuggestion(
+      `https://www.boredapi.com/api/activity?participants=${pplAmount}&type=${catSelection}`
+    );
+  } else if (pplAmount === "" && catSelection !== "") {
+    //if no people
+    return getSuggestion(
+      `https://www.boredapi.com/api/activity?type=${catSelection}`
+    );
+  } else if (pplAmount === "" && catSelection !== "") {
+    //if no category
+    return getSuggestion(
+      `https://www.boredapi.com/api/activity?participants=${pplAmount}`
+    );
+  } else {
+    return getSuggestion(`https://www.boredapi.com/api/activity`);
+  }
+};
+
+//QUOTE https://api.goprogram.ai/inspiration
+const getQuote = () => {
+  axios
+    .get("https://api.goprogram.ai/inspiration")
+    .then((result) => {
+      console.log(result.data);
+      quoteTxt.innerText = result.data.quote;
+      quoteAuthor.innerText = result.data.author;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+getQuote();
